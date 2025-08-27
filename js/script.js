dcd9a141e8f00c0937d4fdaec972537626d202c7
am-mahmud <am.mahmudasif@gmail.com>
2025-08-26 20:17:23 -0400

    Button fuctionality added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,43 @@
-// Navbar buttons
-
-const navButtons = document.getElementsByClassName("btn-nav");
-
-for (let navButton of navButtons ){
-     navButton.addEventListener('click', function() {
-    console.log("btn-clicked");
-  });
+function getElement(id) {
+  const element = document.getElementById(id);
+  return element;
 }
+
+//loveCount 
+
+let totalLoveCount = 0;
+
+getElement("cards-container").addEventListener("click", function (e){
+    if (e.target.className.includes("btn-love")){
+        totalLoveCount += 1;
+        
+        const loveCountElement = document.querySelector(".love-count");
+
+        loveCountElement.innerText = totalLoveCount;
+    }
+});
+
+let totalCoinCount = 100;
+
+getElement("cards-container").addEventListener("click", function (e) {
+  if (e.target.className.includes("btn-call")) {
+    const callButton = e.target;
+
+    if (totalCoinCount < 20) {
+      alert("No balance");
+      return;
+    }
+
+    const serviceName = callButton.parentNode.parentNode.parentNode.children[1].innerText;
+    console.log(serviceName);
+    const serviceNumber = callButton.parentNode.parentNode.parentNode.children[3].innerText;
+    console.log(serviceNumber);
+
+    alert(`Each call will cut 20 coins.\nService Name: ${serviceName}\nService Number: ${serviceNumber}`);
+
+    totalCoinCount = totalCoinCount - 20;
+
+    const coinCountElement = document.querySelector(".coin-count");
+    coinCountElement.innerText = totalCoinCount; 
+  }
+});
