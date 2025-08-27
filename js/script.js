a0d04509d07f43c84e347619a70600a0bf0fd909
am-mahmud <am.mahmudasif@gmail.com>
2025-08-27 00:56:31 -0400

    Copy button funtion added

--- js/script.js
+++ js/script.js
@@ -17,6 +17,7 @@ getElement("cards-container").addEventListener("click", function (e){
     }
 });
 
+// Coin 
 let totalCoinCount = 100;
 
 getElement("cards-container").addEventListener("click", function (e) {
@@ -39,5 +40,33 @@ getElement("cards-container").addEventListener("click", function (e) {
 
     const coinCountElement = document.querySelector(".coin-count");
     coinCountElement.innerText = totalCoinCount; 
+  }
+});
+
+// Copy
+
+let totalCopyCount = 0;
+
+getElement("cards-container").addEventListener("click", function (e) {
+  if (e.target.className.includes("btn-copy")) {
+    const copyButton = e.target;
+    
+    const serviceName = callButton.parentNode.parentNode.parentNode.children[1].innerText;
+    console.log(serviceName);
+
+    const serviceNumber = copyButton.parentNode.parentNode.parentNode.children[3].innerText;
+    console.log(serviceNumber);
+
+    navigator.clipboard.writeText(serviceNumber);
+
+    totalCopyCount += 1;
+    
+    const copyCountParent = document.querySelector(".copy-count");
+
+    const copyCountChild = copyCountParent.children[0];
+
+    copyCountChild.innerText = totalCopyCount;
+    
+  
   }
 });
